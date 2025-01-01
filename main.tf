73e18969c062438841cdbb4e6dce95e07e83a2a4
ptonini <pedro.tonini@gmail.com>
2024-12-31 21:16:27 -0300

    chore: first release

---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_private_dns_zone" "this" {
+  provider            = azurerm
+  name                = var.name
+  resource_group_name = var.rg.name
+  lifecycle {
+    ignore_changes = [
+      tags["business_unit"],
+      tags["environment"],
+      tags["product"],
+      tags["subscription_type"]
+    ]
+  }
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "this" {
+  provider              = azurerm
+  for_each              = var.vnets
+  name                  = "${each.value.name}_${var.name}"
+  private_dns_zone_name = azurerm_private_dns_zone.this.name
+  virtual_network_id    = each.value.id
+  resource_group_name   = var.rg.name
+  lifecycle {
+    ignore_changes = [
+      tags["business_unit"],
+      tags["environment"],
+      tags["product"],
+      tags["subscription_type"]
+    ]
+  }
+}
